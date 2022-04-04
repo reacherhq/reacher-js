9b33eae8ede1eefa59dba2e7d702dbb2d9038c4b
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-04-04 21:02:42 +0200

    fix: Lower default concurrency to 2

--- src/batch.ts
+++ src/batch.ts
@@ -16,6 +16,8 @@ const l = debug('reacher:batch');
 export interface CheckBatchOptions extends CheckSingleOptions {
 	/**
 	 * Concurrency of the queue.
+	 *
+	 * @default 2
 	 */
 	concurrency?: number;
 	/**
@@ -25,10 +27,12 @@ export interface CheckBatchOptions extends CheckSingleOptions {
 }
 
 /**
- * Default number of concurrent API calls. 100 is a good number to use with
- * Reacher.
+ * Default number of concurrent API calls. 2 is a good number to use with
+ * Reacher. Please don't put a too high number, as it will timeout on Reacher.
+ * If you're self-hosting and have the infrastructure, feel free to pump this
+ * number up.
  */
-const DEFAULT_CONCURRENCY = 100;
+const DEFAULT_CONCURRENCY = 2;
 
 /**
  * This function takes an array of emails, and performs batch email
